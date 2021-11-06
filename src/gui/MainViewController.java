f1df57b1cf3fa47a238ca47a027a41edc50e2e2c
Thiago <thiago12prata@gmail.com>
2021-11-05 22:39:36 -0300

    Criado metodo carregarView e carregando a viw Sobre

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable{
 	
@@ -25,12 +34,32 @@ public class MainViewController implements Initializable{
 		System.out.println("teste de ação com botão menuItemDepartamento");
 	}
 	@FXML
-	public void onMmenuItemSobreAction() {
-		System.out.println("teste de ação com botão menuItemSobre");
+	public void onMenuItemSobreAction() {
+		carregarView("/gui/sobre.fxml");
 	}
 	
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
+		
+	}
+	
+	//carrega a view na no scroll pane principal
+	private synchronized void carregarView(String nomeCompletoView) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(nomeCompletoView));
+			VBox novoVbox = loader.load();
+			
+			Scene mainScene = Main.getMainScene();
+			VBox mainVBox = (VBox)((ScrollPane)mainScene.getRoot()).getContent();
+			
+			Node mainMenu = mainVBox.getChildren().get(0);
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(novoVbox.getChildren());
+		}
+		catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Erro ao carregar a View", e.getMessage(), AlertType.ERROR);
+		}
 		
 	}
 }
